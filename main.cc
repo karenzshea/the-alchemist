a4a119660cc5aa3b91357831be2185c11b8f200b
karenzshea <karenzshea@gmail.com>
2016-05-24 18:28:04 -0400

    prepend chunk sizes

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include <protozero/pbf_writer.hpp>
+#include <protozero/varint.hpp>
 
 using AutoclosingFile = std::unique_ptr<::FILE, decltype(&::fclose)>;
 
@@ -12,11 +13,16 @@ int main() {
 
     using Buffer=std::string;
     Buffer thebytes;
-    protozero::pbf_writer pbf(thebytes);
+    protozero::pbf_writer bytespbf(thebytes);
 
-    pbf.add_uint64(1, 42);
-    pbf.add_uint64(2, 590);
-    pbf.add_uint32(3, 60);
+    bytespbf.add_uint64(1, 42);
+    bytespbf.add_uint64(2, 590);
+    bytespbf.add_uint32(3, 60);
+
+    Buffer thesize;
+    protozero::write_varint(std::back_inserter(thesize), 3);
+    protozero::write_varint(std::back_inserter(thesize), 1);
+
     const auto count = ::fwrite(thebytes.data(), sizeof(char), thebytes.size(), testOutputFile.get());
     if (count != thebytes.size()) std::quick_exit(EXIT_FAILURE);
     ::puts("tada\n");
