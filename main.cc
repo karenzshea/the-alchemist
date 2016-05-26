3ebe50a52d7df23167296ba94b4a38bb13fa2748
karenzshea <karenzshea@gmail.com>
2016-05-26 13:54:36 -0400

    go back and write bundle count to header

--- main.cc
+++ main.cc
@@ -40,29 +40,32 @@ int main() {
     }
     writetoFile(bundle_count, testOutputFile.get());
 
+    // write repeated bundles from csv instream
+    std::size_t bundle_i = 0;
     {
-        std::array<csv::Line, lines_per_bundle> unencodedLines;
+        std::vector<csv::Line> unencodedLines;
 
         std::size_t i = 0;
         csv::forEachLine(std::cin, [&](auto&& line) {
             if (i < 1000)
             {
-                unencodedLines[i] = line;
+                unencodedLines.push_back(line);
                 i++;
             } else {
                 // encode chunked lines into messages, and write bundle to file
                 Buffer bundle;
                 {
                     protozero::pbf_writer bundlepbf(bundle);
-                    for (int j = 0; unencodedLines.size(); ++j) {
+                    for (int j = 0; j < unencodedLines.size(); ++j) {
                         encodeLine(bundlepbf, unencodedLines[j]);
                     }
                 }
                 writetoFile(bundle, testOutputFile.get());
+                bundle_i++;
                 // reset
                 i = 0;
                 unencodedLines = {};
-                unencodedLines[i] = line;
+                unencodedLines.push_back(line);
             }
         });
         if (unencodedLines.size() > 0)
@@ -71,12 +74,21 @@ int main() {
             Buffer bundle;
             {
                 protozero::pbf_writer bundlepbf(bundle);
-                for (int j = 0; unencodedLines.size(); ++j) {
+                for (int j = 0; j < unencodedLines.size(); ++j) {
                     encodeLine(bundlepbf, unencodedLines[j]);
                 }
             }
             writetoFile(bundle, testOutputFile.get());
+            bundle_i++;
         }
     }
+    // overwrite initial bundle_count placeholder
 
+    bundle_count.clear();
+    {
+        protozero::pbf_writer bundleCountpbf(bundle_count);
+        bundleCountpbf.add_fixed64(1, bundle_i);
+    }
+    fseek(testOutputFile.get(), 0, SEEK_SET);
+    writetoFile(bundle_count, testOutputFile.get());
 }
