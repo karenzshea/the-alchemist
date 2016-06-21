161d58544c9bebf5ce8864d196286d23091aaa57
karenzshea <karenzshea@gmail.com>
2016-06-21 10:10:27 -0400

    read input file from cmd line

--- decode.cc
+++ decode.cc
@@ -18,14 +18,20 @@ void writetoFile(const std::string& bytes, ::FILE* FileToWrite)
     if (count != bytes.size()) std::quick_exit(EXIT_FAILURE);
 }
 
-int main() {
+int main(int argc, const char *argv[]) {
+    if (argc != 2)
+    {
+        std::cout << "Usage: provide a pbf file to decode\n e.g.: " << argv[0] << " ./file.pbf" << std::endl;
+        std::exit(0);
+    }
+
     AutoclosingFile decodedFile(::fopen("out.csv", "wb"), &::fclose);
     if (!decodedFile) std::quick_exit(EXIT_FAILURE);
 
-    auto infile = mapping::fromReadOnlyFile("out.pbf");
+    auto infile = mapping::fromReadOnlyFile(argv[1]);
 
-    const char* end = reinterpret_cast<const char*>(infile.get_address())+infile.get_size();
-    const char* pos = reinterpret_cast<const char*>(infile.get_address());
+    const auto* end = reinterpret_cast<const char *>(infile.get_address())+infile.get_size();
+    const auto* pos = reinterpret_cast<const char *>(infile.get_address());
 
     while (pos != end) {
         uint64_t chunksize = protozero::decode_varint(
