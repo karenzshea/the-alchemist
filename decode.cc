4a9dc697f6db71f028605f4820c4133bd21dc5b1
Daniel J. Hofmann <daniel@trvx.org>
2016-06-23 15:01:14 +0200

    Clang-Format

--- decode.cc
+++ decode.cc
@@ -1,66 +1,64 @@
 #include <cstdio>
+#include <fstream>
 #include <ios>
 #include <iostream>
-#include <fstream>
 #include <memory>
 
-#include <protozero/pbf_reader.hpp>
 #include <protozero/pbf_message.hpp>
+#include <protozero/pbf_reader.hpp>
 #include <protozero/varint.hpp>
 
 #include "mapping.h"
 
 using AutoclosingFile = std::unique_ptr<::FILE, decltype(&::fclose)>;
-void writetoFile(const std::string& bytes, ::FILE* FileToWrite)
-{
-    const auto count = ::fwrite(bytes.data(),
-            sizeof(char), bytes.size(), FileToWrite);
-    if (count != bytes.size()) std::quick_exit(EXIT_FAILURE);
+void writetoFile(const std::string &bytes, ::FILE *FileToWrite) {
+  const auto count = ::fwrite(bytes.data(), sizeof(char), bytes.size(), FileToWrite);
+  if (count != bytes.size())
+    std::quick_exit(EXIT_FAILURE);
 }
 
 int main(int argc, const char *argv[]) {
-    if (argc != 2)
-    {
-        std::cout << "Usage: provide a pbf file to decode\n e.g.: " << argv[0] << " ./file.pbf" << std::endl;
-        std::exit(0);
-    }
+  if (argc != 2) {
+    std::cout << "Usage: provide a pbf file to decode\n e.g.: " << argv[0] << " ./file.pbf" << std::endl;
+    std::exit(0);
+  }
 
-    AutoclosingFile decodedFile(::fopen("out.csv", "wb"), &::fclose);
-    if (!decodedFile) std::quick_exit(EXIT_FAILURE);
+  AutoclosingFile decodedFile(::fopen("out.csv", "wb"), &::fclose);
+  if (!decodedFile)
+    std::quick_exit(EXIT_FAILURE);
 
-    auto infile = mapping::fromReadOnlyFile(argv[1]);
+  auto infile = mapping::fromReadOnlyFile(argv[1]);
 
-    const auto* end = reinterpret_cast<const char *>(infile.get_address())+infile.get_size();
-    const auto* pos = reinterpret_cast<const char *>(infile.get_address());
+  const auto *end = reinterpret_cast<const char *>(infile.get_address()) + infile.get_size();
+  const auto *pos = reinterpret_cast<const char *>(infile.get_address());
 
-    while (pos != end) {
-        uint64_t chunksize = protozero::decode_varint(
-                &pos, end);
+  while (pos != end) {
+    uint64_t chunksize = protozero::decode_varint(&pos, end);
 
-        protozero::pbf_reader bundle(pos, chunksize);
-        while (bundle.next(1)) {
-            protozero::pbf_reader line = bundle.get_message();
-            uint64_t from, to;
-            uint32_t speed;
-            while (line.next()) {
-                switch(line.tag()) {
-                    case 1:
-                        from = line.get_uint64();
-                        writetoFile(std::to_string(from) + ",", decodedFile.get());
-                        break;
-                    case 2:
-                        to = line.get_uint64();
-                        writetoFile(std::to_string(to) + ",", decodedFile.get());
-                        break;
-                    case 3:
-                        speed = line.get_uint32();
-                        writetoFile(std::to_string(speed) + "\n", decodedFile.get());
-                        break;
-                    default:
-                        line.skip();
-                }
-            }
+    protozero::pbf_reader bundle(pos, chunksize);
+    while (bundle.next(1)) {
+      protozero::pbf_reader line = bundle.get_message();
+      uint64_t from, to;
+      uint32_t speed;
+      while (line.next()) {
+        switch (line.tag()) {
+        case 1:
+          from = line.get_uint64();
+          writetoFile(std::to_string(from) + ",", decodedFile.get());
+          break;
+        case 2:
+          to = line.get_uint64();
+          writetoFile(std::to_string(to) + ",", decodedFile.get());
+          break;
+        case 3:
+          speed = line.get_uint32();
+          writetoFile(std::to_string(speed) + "\n", decodedFile.get());
+          break;
+        default:
+          line.skip();
         }
-        pos += chunksize;
+      }
     }
+    pos += chunksize;
+  }
 }
--- encode.cc
+++ encode.cc
@@ -1,101 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <memory>
 #include <iostream>
+#include <memory>
 
-#include <protozero/varint.hpp>
 #include <protozero/pbf_writer.hpp>
+#include <protozero/varint.hpp>
 
 #include "csv.h"
 
 using AutoclosingFile = std::unique_ptr<::FILE, decltype(&::fclose)>;
-using Buffer=std::string;
+using Buffer = std::string;
 const constexpr std::size_t lines_per_bundle = 1000;
 
-void encodeLine(protozero::pbf_writer& parentpbf, const csv::Line& lineData)
-{
-    protozero::pbf_writer linepbf(parentpbf, 1);
+void encodeLine(protozero::pbf_writer &parentpbf, const csv::Line &lineData) {
+  protozero::pbf_writer linepbf(parentpbf, 1);
 
-    linepbf.add_uint64(1, lineData.from);
-    linepbf.add_uint64(2, lineData.to);
-    linepbf.add_uint32(3, lineData.speed);
+  linepbf.add_uint64(1, lineData.from);
+  linepbf.add_uint64(2, lineData.to);
+  linepbf.add_uint32(3, lineData.speed);
 }
 
-void writetoFile(const Buffer& bytes, ::FILE* FileToWrite)
-{
-    const auto count = ::fwrite(bytes.data(),
-            sizeof(char), bytes.size(), FileToWrite);
-    if (count != bytes.size()) std::quick_exit(EXIT_FAILURE);
+void writetoFile(const Buffer &bytes, ::FILE *FileToWrite) {
+  const auto count = ::fwrite(bytes.data(), sizeof(char), bytes.size(), FileToWrite);
+  if (count != bytes.size())
+    std::quick_exit(EXIT_FAILURE);
 }
 
-void writeHeader(const std::size_t bundle_count, ::FILE* FileToWrite)
-{
-    auto size = static_cast<std::uint64_t>(bundle_count);
-    Buffer buffer;
-    auto it = std::back_inserter(buffer);
-    protozero::write_varint(it, size);
-    writetoFile(buffer, FileToWrite);
+void writeHeader(const std::size_t bundle_count, ::FILE *FileToWrite) {
+  auto size = static_cast<std::uint64_t>(bundle_count);
+  Buffer buffer;
+  auto it = std::back_inserter(buffer);
+  protozero::write_varint(it, size);
+  writetoFile(buffer, FileToWrite);
 }
 
-Buffer encodeGroup(const std::vector<csv::Line>& unencodedLines)
-{
-    Buffer group;
+Buffer encodeGroup(const std::vector<csv::Line> &unencodedLines) {
+  Buffer group;
+  {
+    protozero::pbf_writer groupPbf(group);
     {
-        protozero::pbf_writer groupPbf(group);
-        {
-            protozero::packed_field_uint64 fromNodes{groupPbf, 1};
-            for (const auto &line : unencodedLines) fromNodes.add_element(line.from);
-        }
-        {
-            protozero::packed_field_uint64 toNodes{groupPbf, 2};
-            for (const auto &line : unencodedLines) toNodes.add_element(line.to);
-        }
-        {
-            protozero::packed_field_uint32 speeds{groupPbf, 3};
-            for (const auto &line : unencodedLines) speeds.add_element(line.speed);
-        }
+      protozero::packed_field_uint64 fromNodes{groupPbf, 1};
+      for (const auto &line : unencodedLines)
+        fromNodes.add_element(line.from);
     }
-    return group;
+    {
+      protozero::packed_field_uint64 toNodes{groupPbf, 2};
+      for (const auto &line : unencodedLines)
+        toNodes.add_element(line.to);
+    }
+    {
+      protozero::packed_field_uint32 speeds{groupPbf, 3};
+      for (const auto &line : unencodedLines)
+        speeds.add_element(line.speed);
+    }
+  }
+  return group;
 }
 
-void writeMessageWithHeader(const std::vector<csv::Line>& LinesToWrite, ::FILE* FileToWrite)
-{
-    // encode chunked lines into packed fields
-    auto bundle = encodeGroup(LinesToWrite);
-    // write header file with bundle size
-    writeHeader(bundle.size(), FileToWrite);
-    // write bundle to file
-    writetoFile(bundle, FileToWrite);
+void writeMessageWithHeader(const std::vector<csv::Line> &LinesToWrite, ::FILE *FileToWrite) {
+  // encode chunked lines into packed fields
+  auto bundle = encodeGroup(LinesToWrite);
+  // write header file with bundle size
+  writeHeader(bundle.size(), FileToWrite);
+  // write bundle to file
+  writetoFile(bundle, FileToWrite);
 }
 
 int main() {
-    AutoclosingFile testOutputFile(::fopen("out.pbf", "wb"), &::fclose);
-    if (!testOutputFile.get()) std::quick_exit(EXIT_FAILURE);
+  AutoclosingFile testOutputFile(::fopen("out.pbf", "wb"), &::fclose);
+  if (!testOutputFile.get())
+    std::quick_exit(EXIT_FAILURE);
 
-    // write repeated bundles from csv instream
-    {
-        std::vector<csv::Line> unencodedLines;
-        unencodedLines.reserve(lines_per_bundle);
+  // write repeated bundles from csv instream
+  {
+    std::vector<csv::Line> unencodedLines;
+    unencodedLines.reserve(lines_per_bundle);
 
-        std::size_t i = 0;
-        csv::forEachLine(std::cin, [&](auto&& line) {
-            if (i < lines_per_bundle)
-            {
-                unencodedLines.push_back(line);
-                i++;
-            } else {
-                writeMessageWithHeader(unencodedLines, testOutputFile.get());
-                // reset
-                i = 0;
-                unencodedLines.clear();
-                unencodedLines.push_back(line);
-                i++;
-            }
-        });
-        if (unencodedLines.size() > 0)
-        {
-            writeMessageWithHeader(unencodedLines, testOutputFile.get());
-        }
+    std::size_t i = 0;
+    csv::forEachLine(std::cin, [&](auto &&line) {
+      if (i < lines_per_bundle) {
+        unencodedLines.push_back(line);
+        i++;
+      } else {
+        writeMessageWithHeader(unencodedLines, testOutputFile.get());
+        // reset
+        i = 0;
+        unencodedLines.clear();
+        unencodedLines.push_back(line);
+        i++;
+      }
+    });
+    if (unencodedLines.size() > 0) {
+      writeMessageWithHeader(unencodedLines, testOutputFile.get());
     }
+  }
 }
